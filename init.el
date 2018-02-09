c60a64658ac1a37ee380fe9221b81f34397176c1
mantzaris <avmantzaris@gmail.com>
2018-02-08 23:13:38 -0500

    finally some color theme and it is really outrageous start lol

--- init.el
+++ init.el
@@ -6,6 +6,32 @@
 (tool-bar-mode 0)
 (menu-bar-mode -1)
 
-(require 'color-theme-buffer-local)
-   (add-hook 'latex-mode 
-     (lambda nil (color-theme-buffer-local 'color-theme-empty-void (current-buffer))))
+
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")
+	                          ("melpa" . "http://melpa.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+(load-theme 'dark-mint t)
+
+
+
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("2675e80e3a35ca1650b21cb5f8254073542108871453e5dc3c45b50f72692159" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
